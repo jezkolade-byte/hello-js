4ecfdbe18b3b344153ae6283e2b85da2f349f9df
jez kolade <jez.kolade@gmail.com>
2026-02-21 13:55:07 +0000

    Add week2-node-express app; ignore node_modules and logs

---
+++ MyProject/intro.js
@@ -0,0 +1,4 @@
+console.log("Jezreel Odeneye");
+console.log("I am 13 years old");
+console.log("My favourite coding language is Python");
+
---
+++ raw-server.js
@@ -0,0 +1,18 @@
+// raw server.js (no NPM needed)
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Hello World!');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+const PORT = 3000;
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
